c175b7e4c9ba1a36ace8dabdede3291b6440c987
snipedeepimpact <sagnik_bose@outlook.com>
2021-10-27 16:58:58 +0200

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        //Constructor to initlaize DBContext
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        //Class which has to be managed by DBContext-->table name is Users
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
